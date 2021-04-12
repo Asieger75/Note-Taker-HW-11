d52285a36698abbc723296daae7908580ec30dba
William Woodall <willwoodall@gmail.com>
2021-04-12 10:41:59 -0400

    punctuation

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
   app.get('/notes', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
   
 
